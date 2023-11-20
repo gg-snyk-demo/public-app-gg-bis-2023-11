c645f9edc1b5159c3eccfee6797397d7b7f9fc89
Carol Danvers <carol-danvers-snyk@protonmail.com>
2023-11-20 10:03:59 +0100

    Updating app.py

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 import antigravity
 
-SEED=3ef900da-5eec-4b16-8afc-c6e64c815684
+CERT="""-----BEGIN DSA PRIVATE KEY-----
+Proc-Type: 4,ENCRYPTED
+DEK-Info: AES-256-CBC,D2927AF173235500F75EB60945256A82
+
+8ksrxBg8337H15RzNEwr1pzbmioVY7lgJkTqBLWLXbSPJW0ReldOmhR4tK1g1iBj
+z0ijMdPKBqIpg24wiE+xiXXjzkiBu0kv+rRC9DD2YasxEzlnXwU1HHcHJRm4Scq0
+VDtmFLz3nzjDwAfD5QiYwXRVCPFCEEq4nqmF1n4o8TcDmsdAz6L31eCLQcK7ie6r
+us2vWrcjAFhjxQd2Am82/9gfku+o2BWkm5wStFMLKDG4DbFG2BhGaaJpccAPWoMa
+uJ8nCPIWy8HBtlVSxvihFN8lwQmJ3AoplugdkG5s+tIln9p49wC8eumZ+uZU/C9A
+mv6WSeVRBYMDpqA65jzz7xTVsbkd5gA1Q+Ox6IXu8hFuFDqO6Duh9L1ZxL1M0cjn
++kdDRXz16prrQFc8j7HL85lGFYFUJr1aCbAFtgZM17bNJERy5a/tosXAbePBCdwZ
+/4b2ai8EdkBaGsyzQ97J8GpFB/CD7Gy0FuHO3WVvj1QAcykPEzaeAtYykKMU+vf0
+FDCmk9m/7kDZsxQVUl5Wuq5bjalfoaOzK6BH3xgEsXbltiiJaXRysMp0+J1vZC5r
+lmtY4L8tDwFe+3LKFnFfkg==
+-----END DSA PRIVATE KEY-----
+"""
 
 def main():
     antigravity.fly()
